5c2c55d285d363a469b8aad19e2c9e02c4da01ce
budb <bjoernbudde@gmail.com>
2016-09-26 20:07:11 +0200

    Weatherupdatetask threading

--- widgets/weather.py
+++ widgets/weather.py
@@ -2,24 +2,29 @@ from PyQt5.QtWidgets import QWidget, QLCDNumber, QLabel
 from PyQt5.QtCore import QSize, Qt, QPoint
 from PyQt5 import QtGui, QtCore
 
-import pyowm, yaml, logging
+import pyowm, yaml, logging, _thread
 
 
 class Weather(QLabel):
 
+    def updateWeatherTask(self, args):
+        with open("config.yml", 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        owm = pyowm.OWM(cfg['weather']['api_key'])
+        observation = owm.weather_at_id(2911298)
+        logging.info('updateWeatherTask ' + str(observation))
+        w = observation.get_weather()
+        self.setText("Weather: " + w.get_detailed_status())
+
     def __init__(self, parent=None):
         super(Weather, self).__init__(parent)
 
-        with open("config.yml", 'r') as ymlfile:
-            cfg = yaml.load(ymlfile)
-
-        owm = pyowm.OWM(cfg['weather']['api_key'])
-        observation = owm.weather_at_id(2911298)
-        self.setText("Test")
-        w = observation.get_weather()
-        self.setText("Weather: " + w.get_detailed_status())
+        self.setText("Updating...")
         self.setMinimumHeight(200)
         self.setMinimumWidth(400)
+
+        #Start blocking weatherupdatetask as thread
+        _thread.start_new_thread(self.updateWeatherTask, (self,))
 
         #transparency
         self.setAutoFillBackground(True)
